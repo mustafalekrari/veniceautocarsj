eee6c008bc305f2b46634d6a0810b9b73d3e9672
mustafalekrari <mustafalekrari@github.com>
2026-06-01 22:23:14 +0100

    SEO complet - favicon, meta tags, Open Graph, Schema.org, robots.txt

--- config/urls.py
+++ config/urls.py
@@ -2,14 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('django-admin/', admin.site.urls),
     path('', include('cars.urls')),
     path('reservations/', include('reservations.urls')),
-    path('auth/', include('accounts.urls')),          # auth + espace client
-    path('admin-panel/', include('accounts.urls')),   # dashboard admin
+    path('auth/', include('accounts.urls')),
+    path('admin-panel/', include('accounts.urls')),
     path('api/', include('cars.api_urls')),
     path('api/reservations/', include('reservations.api_urls')),
+    # SEO
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
